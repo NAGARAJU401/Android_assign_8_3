e61561d847ea1941cb182d8fec29781e29161af7
NAGARAJU401 <aadepu.nagaraj@gmail.com>
2018-01-25 11:09:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/android_assign_8_3/MyClass.java
@@ -0,0 +1,12 @@
+package com.example.user.android_assign_8_3;
+
+/**
+ * Created by USER on 25-01-2018.
+ */
+class MyClass {
+    int versionImage;
+    //version of the image to be taken that we write separate class and represent that to the current image
+    public MyClass(int versionImage){
+        this.versionImage=versionImage;
+    }
+}
